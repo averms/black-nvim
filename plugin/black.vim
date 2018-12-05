39c64d618e55b4cd1a1618cf42772d7cf77f0848
Aman <amanraoverma@gmail.com>
2018-12-05 11:21:03 -0500

    Config-parsing and filetype-parsing updated
    Used the object-oriented api to get option variables instead of
    evaling vimscript.
    Also now check if we are in a python file before sending input to black.

--- plugin/black.vim
+++ plugin/black.vim
@@ -11,14 +11,5 @@ let g:load_black = 1
 if !exists("g:black_virtualenv")
   let g:black_virtualenv = g:python3_host_prog
 endif
-if !exists("g:black_fast")
-  let g:black_fast = 0
-endif
-if !exists("g:black_linelength")
-  let g:black_linelength = 88
-endif
-if !exists("g:black_skip_string_normalization")
-  let g:black_skip_string_normalization = 0
-endif
 
 command! Black call Black()
--- rplugin/python3/blacken.py
+++ rplugin/python3/blacken.py
@@ -7,11 +7,12 @@ import sys
 import logging
 
 import pynvim
+
 try:
     import black
 except ImportError:
     print(
-        "Black is not installed in your g:python3_host_prog, please install it with pip and try again",
+        "[**] Black is not installed in your g:python3_host_prog, please install it with pip and try again",
         file=sys.stderr,
     )
     sys.exit(1)
@@ -20,38 +21,36 @@ except ImportError:
 @pynvim.plugin
 class Main:
     def __init__(self, nvim):
-        self.nvim = nvim
+        self.vi = nvim
 
     @pynvim.function("Black")
     def black(self, args):
+        if self.vi.current.buffer.options.get("filetype") != "python":
+            self.vi.out_write("Not in a python file.\n")
+            return
+
         start = time.time()
         black_opts = {
-            "fast": self.get_var("g:black_fast"),
-            "line_length": self.get_var("g:black_linelength"),
+            "fast": bool(self.vi.vars.get("black_fast", False)),
+            "line_length": self.vi.vars.get("black_linelength", 88),
             "mode": black.FileMode.AUTO_DETECT,
         }
-        if self.get_var("g:black_skip_string_normalization"):
+        if self.vi.vars.get("black_skip_string_normalization"):
             black_opts["mode"] |= black.FileMode.NO_STRING_NORMALIZATION
 
-        buf_str = "\n".join(self.nvim.current.buffer) + "\n"
+        buf_str = "\n".join(self.vi.current.buffer) + "\n"
+        self.format_buff(buf_str, black_opts, start)
 
-        self.format(buf_str, black_opts, start)
-
-
-    def format(self, to_format, opts, start):
+    def format_buff(self, to_format, opts, start):
         try:
             new_buffer_str = black.format_file_contents(to_format, **opts)
         except black.NothingChanged:
-            self.nvim.out_write(f"Already well formatted, good job. (took {time.time() - start:.4f}s)\n")
+            self.vi.out_write(
+                f"Already well formatted, good job. (took {time.time() - start:.4f}s)\n"
+            )
         else:
-            cursor = self.nvim.current.window.cursor
-            self.nvim.current.buffer[:] = new_buffer_str.split("\n")[:-1]
-            self.nvim.current.window.cursor = cursor
-            self.nvim.out_write(f"Reformatted in {time.time() - start:.4f}s.\n")
+            cursor = self.vi.current.window.cursor
+            self.vi.current.buffer[:] = new_buffer_str.split("\n")[:-1]
+            self.vi.current.window.cursor = cursor
+            self.vi.out_write(f"Reformatted in {time.time() - start:.4f}s.\n")
 
-
-    def get_var(self, vimscript_exp):
-        # try:
-            return self.nvim.eval(vimscript_exp)
-        # except pynvim.api.nvim.NvimError:
-        #     return 0
