9c7da1e4052813d4e5004f8c6f96dd738fed6b22
Aman <amanraoverma@gmail.com>
2019-01-12 05:31:07 -0500

    Add static type annotations.

--- rplugin/python3/blacken.py
+++ rplugin/python3/blacken.py
@@ -4,17 +4,18 @@
 # ------------------------------------------------------------------------------
 import time
 import sys
+from typing import List, Dict, Union
 
 # doesn't work?
 # import logging
-
 import pynvim
 
 try:
     import black
 except ImportError:
     print(
-        "[**] Black is not installed in your g:python3_host_prog, please install it with pip and try again",
+        "[**] Black is not installed in your g:python3_host_prog, "
+        "please install it with pip and try again",
         file=sys.stderr,
     )
     sys.exit(1)
@@ -22,28 +23,33 @@ except ImportError:
 
 @pynvim.plugin
 class Main:
-    def __init__(self, nvim):
+    def __init__(self, nvim: pynvim.api.Nvim):
         self.vi = nvim
 
     @pynvim.function("Black")
-    def black(self, args):
+    def black(self, args: List[str]) -> None:
         if self.vi.current.buffer.options.get("filetype") != "python":
-            self.vi.out_write("Not in a python file.\n")
+            self.vi.err_write("Not in a python file.\n")
             return
 
         start = time.time()
-        black_opts = {
+        options = self._get_opts()
+        buf_str = "\n".join(self.vi.current.buffer) + "\n"
+        self._format_buff(buf_str, options, start)
+
+    def _get_opts(self) -> Dict[str, Union[int, bool]]:
+        options = {
             "fast": bool(self.vi.vars.get("black_fast", False)),
             "line_length": self.vi.vars.get("black_linelength", 88),
             "mode": black.FileMode.AUTO_DETECT,
         }
         if self.vi.vars.get("black_skip_string_normalization"):
-            black_opts["mode"] |= black.FileMode.NO_STRING_NORMALIZATION
+            options["mode"] |= black.FileMode.NO_STRING_NORMALIZATION
+        return options
 
-        buf_str = "\n".join(self.vi.current.buffer) + "\n"
-        self.format_buff(buf_str, black_opts, start)
-
-    def format_buff(self, to_format, opts, start):
+    def _format_buff(
+        self, to_format: str, opts: Dict[str, Union[int, bool]], start: float
+    ) -> None:
         try:
             new_buffer_str = black.format_file_contents(to_format, **opts)
         except black.NothingChanged:
